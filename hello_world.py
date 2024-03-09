3158457d1b6b274c3089a8c46f6adeaa524933a5
Diego Saavedra <dsaavedra88@gmail.com>
2024-03-08 20:46:59 -0500

    se agrego atencion

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,8 @@
 def welcome_message():
 		name = input("Por favor ingrese su nombre: ")
+		atencion = input("Que fue lo que mas te llamo la atencion del curso: ")
 		print("Bienvenido,", name, "al curso de Django y React!")
+		print("Espero que aprendas", atencion)
 														
 if __name__ == "__main__":
 	welcome_message()
