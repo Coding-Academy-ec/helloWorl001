f77a30e3ba7135a283c6ed00bd0830a65c7d6a23
Diego Saavedra <dsaavedra88@gmail.com>
2024-03-08 20:44:31 -0500

    Agregue Ciudad

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,8 @@
 def welcome_message():
 		name = input("Por favor ingrese su nombre: ")
+		ciudad = input("Por favor agregue su ciudad: ")
 		print("Bienvenido,", name, "al curso de Django y React!")
+		print("Espero te encuentre bien en", ciudad)
 														
 if __name__ == "__main__":
 	welcome_message()
